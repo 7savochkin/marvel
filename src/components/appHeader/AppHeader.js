915d984ce36b72c997a199b4e89f23edb11f3d14
savochkin <savochkinoleg17@gmail.com>
2023-11-07 21:04:34 +0200

    routes

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,19 @@
 import './appHeader.scss';
+import {Link, NavLink} from "react-router-dom";
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="https://www.marvel.com/">
+                <Link to="/">
                     <span>Marvel</span>
                     information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="https://www.marvel.com/characters">Characters</a></li>
-                    <li><a href="https://www.marvel.com/comics?&options%5Boffset%5D=0&totalcount=12">Comics</a></li>
+                    <li><NavLink exact activeStyle={{color: '#9f0013'}} to="/">Characters</NavLink></li>
+                    <li><NavLink exact activeStyle={{color: '#9f0013'}} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
